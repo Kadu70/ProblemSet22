a22192417bb625486d6601d80a97ec7752ee8044
ALUNO <ALUNO@LAB04-PC04>
2024-06-04 21:39:10 -0300

    Java_Foundations

---
+++ src/ProblemSet2_2.java
@@ -0,0 +1,46 @@
+public class ProblemSet2_2 {
+    public static void main(String[] args) {
+        //Use print statements to create your own beautiful original ASCII art.
+        //Use comments to describe what your image is depicting.
+        
+        System.out.println(" ################### ");
+        System.out.println("#                   #");
+        System.out.println("#  00  | x x | 00   #");
+        System.out.println("# 0  0 |  x  |0  0  #");
+        System.out.println("#  00  | x x | 00   #");
+        System.out.println("# ----------------- #");
+        System.out.println("#  x x | x x |x x   #");
+        System.out.println("# --x-----x----x--  #");
+        System.out.println("#  x x | x x |x x   #");
+        System.out.println("# ----------------- #");
+        System.out.println("#      | 00  | 00   #");
+        System.out.println("#      |0  0 |0  0  #");
+        System.out.println("#      | 00  | 00   #");
+        System.out.println("#                   #");
+        System.out.println(" ################### ");
+        
+         System.out.println(" \n\n\n ");
+        
+        System.out.println("         ##           ");
+        System.out.println("        ####          ");
+        System.out.println("     ##########       ");
+        System.out.println("   #            #     ");
+        System.out.println("  ##  O      O  ##    ");
+        System.out.println("  ##      V     ##    ");
+        System.out.println("    #  *    *  #      ");
+        System.out.println("     ##  **  ##       ");
+        System.out.println("       ######         ");
+
+        
+                
+
+                
+        
+        
+        
+        
+        
+        
+        
+    } 
+}
